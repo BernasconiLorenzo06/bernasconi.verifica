73d170d17112e680b25cad6d0efb2c0d28263daa
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2025-03-28 12:58:36 +0000

    commit finale

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var pagesRouter = require('./routes/pages');
 var app = express();
 
 // view engine setup
@@ -19,8 +19,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/rest', usersRouter);
+app.use('/pages', pagesRouter);
+
+app.use(function(req, res, next) {
+  if (req.path.startsWith('/rest') && !req.path.startsWith('/pages')) {
+    return next(createError(404, 'Route not found'));
+  }
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+const people = require('../persone.json');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/users', function(req, res, next) {
+  
+  res.send(people);
 
+});
 module.exports = router;
